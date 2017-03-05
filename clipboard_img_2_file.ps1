32bb2742283f3a4001935abd0f2e4151a0d7d1e4
j0t4 <j0t4@j0t4.com>
2017-03-05 16:43:20 +0100

    Add files via upload

---
+++ clipboard_img_2_file.ps1
@@ -0,0 +1,27 @@
+﻿#Add-Type -Assembly PresentationCore
+$img = [Windows.Clipboard]::GetImage()
+if ($img -eq $null) {
+    Write-Host "Imagen no encontrada."
+    Exit
+}
+Write-Host ("Imagen encontrada. {0}x{1} Pixels." -f $img.PixelWidth,$img.PixelHeight)
+
+$fcb = new-object Windows.Media.Imaging.FormatConvertedBitmap($img, [Windows.Media.PixelFormats]::Rgb24, $null, 0)
+
+$encoder = New-Object Windows.Media.Imaging.PngBitmapEncoder
+$encoder.Frames.Add([Windows.Media.Imaging.BitmapFrame]::Create($fcb))
+
+#  a través de un archivo en disco.....
+#$path = "{0}\Clipboard-{1}.png" -f [System.Environment]::GetFolderPath('MyPictures'),((Get-Date -f s) -replace '[-T:]','')
+#$stream = [IO.File]::Open($path, "OpenOrCreate")
+#$encoder.Save($stream)
+#$stream.Dispose()
+#[convert]::ToBase64String((get-content $path -encoding byte))
+
+
+# a través de memoria : memorystream
+$mi_stream=New-Object System.IO.MemoryStream 
+$encoder.Save($mi_stream)
+$IMG_BASE64 = [convert]::ToBase64String($mi_stream.GetBuffer())
+$mi_stream.Dispose()
+
---
+++ word_img_2_clipboard.ps1
@@ -0,0 +1,20 @@
+﻿$Word = New-Object -ComObject Word.Application
+$Document=$Word.documents.open("c:\tmp\worddoc.docx")
+
+$paras = $Document.Paragraphs
+write-host $paras.count
+
+foreach ($para in $paras) 
+{ 
+     Write-Host ("-"+$para.Range.Text+"-")
+     if ($para.Range.inlineshapes.count -gt 0){
+          write-host  $para.Range.InlineShapes[1].Width
+         $para.Range.InlineShapes[1].Width = 600
+         #$para.Range.InlineShapes[1].ScaleHeight =100
+
+         $para.Range.InlineShapes[1].Range.Copy()
+     }
+}
+
+$document.close()
+$word.quit()
